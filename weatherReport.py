c37ba72d4fff8facf1156dba47d58586c8507c91
anhkhoido <anhkhoido@gmail.com>
2021-02-25 21:18:21 -0500

    Refactored method named get_current_weather_in(city).

--- weatherReport.py
+++ weatherReport.py
@@ -25,16 +25,25 @@ def generate_json_for_parsing(response):
     json_data = response.read()
     return json.loads(json_data)
 
+def display_weather_report(city, longitude, latitude, temperature, humidity_index, wind_speed):
+    print(f"City: {city}")
+    print(f"Longitude: {longitude}")
+    print(f"Latitude: {latitude}")
+    print(f"Temperature: {temperature} degrees Celcius.")
+    print(f"Humidex: {humidity_index}")
+    print(f"Wind speed: {wind_speed} km/h")
+
 def get_current_weather_in(city):
     response = request_weather_report(OPENWEATHERMAP_URL + str("q=" + city + "&units=metric") + OPENWEATHERMAP_API_KEY)
     if response.getcode() == 200:
         json_result = generate_json_for_parsing(response)
-        print("City: " + json_result["name"])
-        print("Longitude: " + get_longitude_of_city(json_result))
-        print("Latitude: " + get_latitude_of_city(json_result))
-        print("Temperature: " + get_temperature(json_result) + " degrees Celcius.")
-        print("Humidex: " + get_humidity_index(json_result))
-        print("Wind speed: " + get_wind_speed(json_result) + " km/h")
+        city = json_result["name"]
+        longitude = get_longitude_of_city(json_result)
+        latitude = get_latitude_of_city(json_result)
+        temperature = get_temperature(json_result)
+        humidity_index = get_humidity_index(json_result)
+        wind_speed = get_wind_speed(json_result)
+        display_weather_report(city, longitude, latitude, temperature, humidity_index, wind_speed)
     else:
         print("Error!")
 
