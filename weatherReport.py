9f59f97ab2ad07c39b2a35ec24e588b882a1d050
anhkhoido <anhkhoido@gmail.com>
2021-02-25 21:43:34 -0500

    Modified .gitignore and added logging.

--- weatherReport.py
+++ weatherReport.py
@@ -1,7 +1,13 @@
-import urllib.request
 import json
+import logging
+import sys
+import urllib.request
 from confidential import OPENWEATHERMAP_URL
 from confidential import OPENWEATHERMAP_API_KEY
+
+logging.basicConfig(level= logging.DEBUG, filename="weatherReport.log", format='%(asctime)s %(message)s', filemode='w')
+
+LOGGER = logging.getLogger()
 
 def get_wind_speed(data):
     return str(data["wind"]["speed"])
@@ -26,6 +32,7 @@ def generate_json_for_parsing(response):
     return json.loads(json_data)
 
 def display_weather_report(city, longitude, latitude, temperature, humidity_index, wind_speed):
+    LOGGER.debug(f"weatherReport.py : display_weather_report({city}, {longitude}, {latitude}, {temperature}, {humidity_index}, {wind_speed})")
     print(f"City: {city}")
     print(f"Longitude: {longitude}")
     print(f"Latitude: {latitude}")
@@ -34,8 +41,11 @@ def display_weather_report(city, longitude, latitude, temperature, humidity_inde
     print(f"Wind speed: {wind_speed} km/h")
 
 def get_current_weather_in(city):
+    LOGGER.info(f"weatherReport.py : get_current_weather_in({city})")
+    LOGGER.debug(f"  HTTP request to: {OPENWEATHERMAP_URL}q={city}&units=metric{OPENWEATHERMAP_API_KEY}")
     response = request_weather_report(OPENWEATHERMAP_URL + str("q=" + city + "&units=metric") + OPENWEATHERMAP_API_KEY)
     if response.getcode() == 200:
+        LOGGER.debug("  HTTP request successful")
         json_result = generate_json_for_parsing(response)
         city = json_result["name"]
         longitude = get_longitude_of_city(json_result)
@@ -45,10 +55,16 @@ def get_current_weather_in(city):
         wind_speed = get_wind_speed(json_result)
         display_weather_report(city, longitude, latitude, temperature, humidity_index, wind_speed)
     else:
-        print("Error!")
+        LOGGER.error("Problem with the server of openweathermap.org.")
 
 def main():
-    get_current_weather_in("Toronto")
+    LOGGER.debug("WeatherReport.py : main()")
+    if len(sys.argv) == 2:
+        city = sys.argv[1]
+        LOGGER.info(f"Analyzing city named {city}.")
+        get_current_weather_in(city)
+    else:
+        LOGGER.error("Wrong number of arguments at the command line.")
 
 if __name__ == "__main__":
     main()
